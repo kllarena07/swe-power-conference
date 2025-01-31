41799fdf08e0af42e49f60c1525dfc57e004baf0
aaronamano <aaroncedric2005@yahoo.com>
2025-01-31 16:53:03 -0500

    created welcome page

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,97 @@
+import { View, Text, Pressable } from 'react-native'
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import powerTitleImg from "@/assets/images/powertitle.png"
+import { Image } from 'react-native'
+import { Link } from 'expo-router'
+
+const index = () => {
+  return (
+
+    <View style={styles.container} className="flex-1 items-center justify-center text-center">
+      <Image source={powerTitleImg} style={styles.image}/>
+
+      <Text style={styles.welcome} className="text-center">Welcome to UMD'S 2025 Power Conference</Text>
+      <Text style={styles.subtext} className="text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium illum labore at amet dolorum delectus cupiditate vero eveniet, distinctio ab quam eum et est voluptate earum optio nostrum eius corrupti?</Text>
+
+
+      <Link href="/signup" asChild>
+        <Pressable style={styles.signInButton}>
+          <Text style={styles.signInButtonText}>Sign Up</Text>
+        </Pressable>
+      </Link>
+
+      <Link href="/login" asChild>
+        <Pressable style={styles.logInButton}>
+          <Text style={styles.logInButtonText}>Log In</Text>
+        </Pressable>
+      </Link>
+
+    </View>
+
+
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#82599a', // Set the background color here
+  },
+  welcome: {
+    color: '#ebcac6',
+    fontFamily: 'Kurale',
+    fontSize: 24,
+    width: 333,
+    height: 144
+  },
+  subtext: {
+    color: '#ebcac6',
+    fontFamily: 'Kurale',
+    fontSize: 14,
+    width: 290,
+    height: 89,
+    transform: [{ translateY: -70 }]
+  },
+  image: {
+    width: 367,
+    height: 186,
+  },
+  signInButton: {
+    backgroundColor: '#ece5d7',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+    width: 309,
+    height: 51,
+    justifyContent: 'center',
+    transform: [{ translateY: -60 }]
+  },
+  signInButtonText: {
+    color: '#82599a',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center'
+    
+  },
+  logInButton: {
+    backgroundColor: '#82599a',
+    padding: 10,
+    borderRadius: 5,
+    borderColor: '#ece5d7',
+    borderWidth: 1,
+    marginTop: 10,
+    width: 309,
+    height: 51,
+    justifyContent: 'center',
+    transform: [{ translateY: -60 }]
+  },
+  logInButtonText: {
+    color: '#ece5d7',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center'
+    
+  },
+});
+
+export default index
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,4 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -25,6 +24,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    Kurale: require('../assets/fonts/Kurale-Regular.ttf'),
     ...FontAwesome.font,
   });
 
@@ -50,11 +50,9 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
-    </ThemeProvider>
   );
 }
