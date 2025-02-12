255eb12c60d85b63bee931aab10e9db1606dbc29
Kieran Llarena <baymax709@gmail.com>
2025-02-11 23:04:16 -0500

    chore: view -> scrollview + repeat MessageCards for ui testing

--- app/main/(tabs)/messages.tsx
+++ app/main/(tabs)/messages.tsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, View, Text } from "react-native";
+import { SafeAreaView, Text, ScrollView } from "react-native";
 import React from "react";
 import MessageCard from "@/components/MessageCard";
 
@@ -6,13 +6,16 @@ export default function Messages() {
   return (
     <SafeAreaView className="bg-white h-full">
       <Text className="text-4xl mt-3.5 font-bold text-center">Messages</Text>
-      <View className="px-10">
-        <MessageCard
-          title="Important"
-          content="Lunch time has been slightly moved to 2pm"
-          time={new Date(new Date().setHours(22, 37, 0))}
-        />
-      </View>
+      <ScrollView className="px-10">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <MessageCard
+            key={index}
+            title="Important"
+            content="Lunch time has been slightly moved to 2pm"
+            time={new Date(new Date().setHours(22, 37, 0))}
+          />
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 }
