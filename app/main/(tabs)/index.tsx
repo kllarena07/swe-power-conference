8114fd14518b0902c58d309cc66f33f0e4e55abe
Kieran Llarena <baymax709@gmail.com>
2025-02-12 16:28:27 -0500

    init: agenda page

---
+++ app/main/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+// This is the agenda page
+
+import { SafeAreaView, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <SafeAreaView>
+      <Text>This is the agenda page.</Text>
+    </SafeAreaView>
+  );
+}
