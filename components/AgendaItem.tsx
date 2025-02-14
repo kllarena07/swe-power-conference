6dc84384c41aa3fc4061bcb753c3c376e13f7546
Kieran Llarena <baymax709@gmail.com>
2025-02-13 21:36:08 -0500

    refactor: integrate AgendaItem component with db

--- components/AgendaItem.tsx
+++ components/AgendaItem.tsx
@@ -1,25 +1,56 @@
 import React from "react";
 import { View, Text } from "react-native";
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 interface AgendaItemProps {
-    time: string;
-    title: string;
-    description: string;
+  start_time: string;
+  end_time: string;
+  title: string;
+  description: string;
+  index: number;
 }
 
-const AgendaItem = ({ time, title, description }: AgendaItemProps) => (
-  <View className="mb-8">
-    <View className="flex-row items-center gap-2">
-        <MaterialCommunityIcons name="star-four-points" size={20} color="#C3A0D7" />
-        <Text className="text-[#8F9BB3] mb-1">{time}</Text>
+const formatTimeString = (time_string: string) => {
+  return new Date(time_string).toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    timeZone: "America/New_York",
+  });
+};
+
+const getStarColor = (index: number) => {
+  const colors = ["#C3A0D7", "#9A598A", "#82599A", "#EBCAC6"];
+  return colors[index % colors.length];
+};
+
+const AgendaItem = ({
+  start_time,
+  end_time,
+  title,
+  description,
+  index,
+}: AgendaItemProps) => {
+  const formattedStartTime = formatTimeString(start_time);
+  const formattedEndTime = formatTimeString(end_time);
+  return (
+    <View className="mb-8">
+      <View className="flex-row items-center gap-2">
+        <MaterialCommunityIcons
+          name="star-four-points"
+          size={20}
+          color={getStarColor(index)}
+        />
+        <Text className="text-[#8F9BB3] mb-1">
+          {formattedStartTime} - {formattedEndTime}
+        </Text>
+      </View>
+      <View>
+        <Text className="text-black text-base font-semibold mb-1">{title}</Text>
+        <Text className="text-[#BDBDBD] text-sm">{description}</Text>
+      </View>
+      <View className="absolute -bottom-4 left-0 right-0 h-[1px] bg-[#E8E8E8]" />
     </View>
-    <View>
-      <Text className="text-black text-base font-semibold mb-1">{title}</Text>
-      <Text className="text-[#BDBDBD] text-sm">{description}</Text>
-    </View>
-    <View className="absolute -bottom-4 left-0 right-0 h-[1px] bg-[#E8E8E8]" />
-  </View >
-);
+  );
+};
 
 export default AgendaItem;
