f2f319a9390939255318d8a5459efd25edca9b54
Kieran Llarena <baymax709@gmail.com>
2025-02-13 19:15:36 -0500

    feat: protected routes under

--- app/protected/(tabs)/_layout.tsx
+++ app/protected/(tabs)/_layout.tsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
-import { Tabs } from "expo-router";
+import { useEffect, useState } from "react";
+import { Tabs, useRouter } from "expo-router";
 import { MaterialIcons } from "@expo/vector-icons";
-import { StatusBar } from "react-native";
+import { StatusBar, Text } from "react-native";
+import { Session } from "@supabase/supabase-js";
+import { supabase } from "@/utils/supabase";
 
 export default function TabLayout() {
+  const router = useRouter();
+  const [session, setSession] = useState<Session | null>(null);
   const [activeRoute, setActiveRoute] = useState("");
 
   const getIconColor = (routeName: string) => {
@@ -12,66 +16,84 @@ export default function TabLayout() {
       : "hsla(0, 0%, 74%, 1)";
   };
 
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    if (!session) {
+      router.replace("/login");
+      return;
+    }
+  }, []);
+
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: {
-          paddingTop: 12,
-          backgroundColor: "hsla(0, 0%, 98%, 1)",
-        },
-      }}
-      screenListeners={{
-        state: (e) => {
-          const routeName = e.data.state.routes[e.data.state.index].name;
+    <>
+      {session && session.user && <Text>{session.user.id}</Text>}
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: {
+            paddingTop: 12,
+            backgroundColor: "hsla(0, 0%, 98%, 1)",
+          },
+        }}
+        screenListeners={{
+          state: (e) => {
+            const routeName = e.data.state.routes[e.data.state.index].name;
 
-          const theme =
-            routeName === "profile" ? "light-content" : "dark-content";
+            const theme =
+              routeName === "profile" ? "light-content" : "dark-content";
 
-          StatusBar.setBarStyle(theme);
-          setActiveRoute(routeName);
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="messages"
-        options={{
-          tabBarLabel: "",
-          tabBarIcon: () => (
-            <MaterialIcons
-              name="mail-outline"
-              size={32}
-              color={getIconColor("messages")}
-            />
-          ),
+            StatusBar.setBarStyle(theme);
+            setActiveRoute(routeName);
+          },
         }}
-      />
-      <Tabs.Screen
-        name="index"
-        options={{
-          tabBarLabel: "",
-          tabBarIcon: () => (
-            <MaterialIcons
-              name="alarm"
-              size={32}
-              color={getIconColor("index")}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          tabBarLabel: "",
-          tabBarIcon: () => (
-            <MaterialIcons
-              name="person-outline"
-              size={32}
-              color={getIconColor("profile")}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="messages"
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: () => (
+              <MaterialIcons
+                name="mail-outline"
+                size={32}
+                color={getIconColor("messages")}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="index"
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: () => (
+              <MaterialIcons
+                name="alarm"
+                size={32}
+                color={getIconColor("index")}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: () => (
+              <MaterialIcons
+                name="person-outline"
+                size={32}
+                color={getIconColor("profile")}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
