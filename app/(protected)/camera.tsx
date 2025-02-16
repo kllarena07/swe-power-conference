42632c62c05d1880124f1631293d9ffe62c77240
Kieran Llarena <baymax709@gmail.com>
2025-02-16 17:07:35 -0500

    refactor: better camera page

--- app/(protected)/camera.tsx
+++ app/(protected)/camera.tsx
@@ -1,10 +1,37 @@
-import { SafeAreaView, Text, View } from "react-native";
+import { CameraView, useCameraPermissions } from "expo-camera";
+import { Button, SafeAreaView, Text, View } from "react-native";
 
-export default function Camera() {
+export default function App() {
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    return (
+      <SafeAreaView>
+        <Text>Attempting to get the user's permission for camera access.</Text>
+      </SafeAreaView>
+    );
+  }
+
+  if (!permission.granted) {
+    return (
+      <View className="flex-1 justify-center">
+        <Text className="text-center pb-2.5">
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
   return (
-    <SafeAreaView className="relative h-full">
-      <Text>This is the camera scanning page.</Text>
+    <View className="relative flex-1 justify-center">
+      <CameraView
+        style={{
+          flex: 1,
+        }}
+        facing="back"
+      />
       <View className="absolute bottom-0 right-0 bg-[hsla(278,41%,74%,1)] w-1/4 h-[1.5px]" />
-    </SafeAreaView>
+    </View>
   );
 }
