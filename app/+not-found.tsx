f454bf4bbd38207431ddcc5455b1d33ad4677c4c
Kieran Llarena <baymax709@gmail.com>
2025-02-17 21:21:16 -0500

    refactor: wrap not found page in safeareaview

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,17 +1,18 @@
 import { Link, Stack } from "expo-router";
 import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: "Oops!" }} />
-      <View className="flex-1 items-center justify-center p-5">
+      <SafeAreaView className="flex-1 items-center justify-center p-5">
         <Text className="text-lg font-bold">This screen doesn't exist.</Text>
 
         <Link href="/" className="mt-3 py-3">
           <Text className="text-sm text-blue-600">Go to home screen!</Text>
         </Link>
-      </View>
+      </SafeAreaView>
     </>
   );
 }
