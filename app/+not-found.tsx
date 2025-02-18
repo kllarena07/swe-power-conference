38cc567f3f9ebed3129f300a22b39251bb4f0938
Kieran Llarena <baymax709@gmail.com>
2025-02-17 21:20:49 -0500

    chore: removed unused components

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,10 +1,10 @@
-import { Link, Stack } from 'expo-router';
-import { Text, View } from '@/components/Themed';
+import { Link, Stack } from "expo-router";
+import { View, Text } from "react-native";
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
+      <Stack.Screen options={{ title: "Oops!" }} />
       <View className="flex-1 items-center justify-center p-5">
         <Text className="text-lg font-bold">This screen doesn't exist.</Text>
 
