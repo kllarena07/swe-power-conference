124cfac9bb22ceabb5d03aa0012d7c850e966447
Kieran Llarena <baymax709@gmail.com>
2025-02-18 23:24:46 -0500

    feat: confetti on winner selection

--- app/(protected)/raffle.tsx
+++ app/(protected)/raffle.tsx
@@ -1,7 +1,7 @@
 import { ProfileData, useAuth } from "@/context/AuthContext";
 import { sendPushNotification } from "@/utils/push-notif";
 import { supabase } from "@/utils/supabase";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import {
   SafeAreaView,
   View,
@@ -9,10 +9,12 @@ import {
   TouchableOpacity,
   Alert,
 } from "react-native";
+import ConfettiCannon from "react-native-confetti-cannon";
 
 export default function Raffle() {
   const [winner, setWinner] = useState<ProfileData["name"]>("");
   const { accessToken } = useAuth();
+  const confettiRef = useRef<ConfettiCannon | null>(null);
 
   const runRaffle = async () => {
     console.log("Running raffle...");
@@ -52,7 +54,10 @@ export default function Raffle() {
 
           if (!response.ok) {
             Alert.alert("Error selecting raffle winner", response.statusText);
+            return;
           }
+
+          if (confettiRef.current) confettiRef.current.start();
         } catch (err: any) {
           Alert.alert("Error selecting raffle winner", err);
         }
@@ -61,13 +66,22 @@ export default function Raffle() {
   };
 
   return (
-    <SafeAreaView className="h-full flex justify-center items-center gap-5">
+    <SafeAreaView className="relative h-full flex justify-center items-center gap-5">
       <Text className="text-3xl font-bold">Raffle Page</Text>
       <TouchableOpacity className="bg-black p-5 rounded-md" onPress={runRaffle}>
         <Text className="text-white font-bold">Draw Winner</Text>
       </TouchableOpacity>
       <Text className="text-lg">The winner is: {winner}</Text>
       <View className="absolute bottom-0 right-0 bg-[hsla(278,41%,74%,1)] w-1/5 h-[1.5px]" />
+      <ConfettiCannon
+        count={200}
+        origin={{ x: 0, y: 0 }}
+        ref={confettiRef}
+        autoStart={false}
+        fallSpeed={2500}
+        explosionSpeed={800}
+        fadeOut={true}
+      />
     </SafeAreaView>
   );
 }
