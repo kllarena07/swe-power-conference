d99ff89cc520dd14fe3b1583c32c5c8be8f5f242
Kieran Llarena <baymax709@gmail.com>
2025-02-18 22:43:29 -0500

    feat: created raffle page and added to tabs layout

---
+++ app/(protected)/raffle.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from "react-native";
+
+export default function Raffle() {
+  return (
+    <SafeAreaView>
+      <Text>This is the raffle page</Text>
+    </SafeAreaView>
+  );
+}
