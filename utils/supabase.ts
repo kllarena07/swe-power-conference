a3c2a64fd21d9f22ac9a947e2df9db4407644825
Kieran Llarena <baymax709@gmail.com>
2025-02-27 12:03:56 -0500

    chore: disable supabase

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,23 +1,23 @@
-import "react-native-url-polyfill/auto";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { createClient } from "@supabase/supabase-js";
-import { Alert } from "react-native";
+// import "react-native-url-polyfill/auto";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+// import { createClient } from "@supabase/supabase-js";
+// import { Alert } from "react-native";
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? "";
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? "";
+// const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? "";
+// const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? "";
 
-if (supabaseUrl === "" || supabaseAnonKey === "") {
-  Alert.alert(
-    "Configuration Error",
-    "Supabase URL or Anonymous Key is missing. Please check your environment configuration.",
-  );
-}
+// if (supabaseUrl === "" || supabaseAnonKey === "") {
+//   Alert.alert(
+//     "Configuration Error",
+//     "Supabase URL or Anonymous Key is missing. Please check your environment configuration.",
+//   );
+// }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    storage: AsyncStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
-});
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+//   auth: {
+//     storage: AsyncStorage,
+//     autoRefreshToken: true,
+//     persistSession: true,
+//     detectSessionInUrl: false,
+//   },
+// });
