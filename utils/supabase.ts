8458a784e5cd9102c821dc2cb77df8f218f8e551
Kieran Llarena <baymax709@gmail.com>
2025-02-27 13:17:29 -0500

    chore: hard code supabase keeys

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,23 +1,16 @@
-// import "react-native-url-polyfill/auto";
-// import AsyncStorage from "@react-native-async-storage/async-storage";
-// import { createClient } from "@supabase/supabase-js";
-// import { Alert } from "react-native";
+import "react-native-url-polyfill/auto";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
 
-// const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? "";
-// const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? "";
+const supabaseUrl = "https://ffzljrapzzjpcjsfupeh.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmemxqcmFwenpqcGNqc2Z1cGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI0NzgwMDUsImV4cCI6MjA0ODA1NDAwNX0.Zbq6AedhycvoEVsmXZpnLIrcaqeYMx46CVpEMoHksrw";
 
-// if (supabaseUrl === "" || supabaseAnonKey === "") {
-//   Alert.alert(
-//     "Configuration Error",
-//     "Supabase URL or Anonymous Key is missing. Please check your environment configuration.",
-//   );
-// }
-
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-//   auth: {
-//     storage: AsyncStorage,
-//     autoRefreshToken: true,
-//     persistSession: true,
-//     detectSessionInUrl: false,
-//   },
-// });
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
